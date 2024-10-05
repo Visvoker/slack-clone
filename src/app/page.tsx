1522a27877f4072f691c9b40742df069acdc9659
visvoker <s2089533@gmail.com>
2024-10-05 10:13:40 +0800

    Auth screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
+
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-rose-500 font-bold text-4xl ">Hello world</div>
-      <Button >Click me</Button>
-    </div>
+    <AuthScreen />
   );
 }
